1aa11829b1d0cd3b0a5d9aafb57dde7e5849068b
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-08-09 12:06:37 -0300

    Calculate Tax & Grand Total

--- carts/views.py
+++ carts/views.py
@@ -44,6 +44,8 @@ def cart(request, total=0, quantity=0, cart_items=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (3 * total)/100
+        grand_total = total + tax
     except ObjectNotExist:
         pass # just ignore
 
@@ -51,5 +53,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
