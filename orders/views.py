03816b493139de4b5a2972e1989318c5fffdcb71
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-08-23 18:19:36 -0300

    Send Order Received Email

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,8 @@ from .models import Order, Payment, OrderProduct
 import datetime
 import json
 from store.models import Product
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -56,7 +58,15 @@ def payments(request):
     # Clear cart
     CartItem.objects.filter(user=request.user).delete()
 
-    # Send order recieved email to customer
+    # Send order received email to customer
+    mail_subject = 'Thank you for your order'
+    message      = render_to_string('orders/order_received_email.html', {
+        'user': request.user,
+        'order': order
+    })
+    to_email   = request.user.email
+    send_email = EmailMessage(mail_subject, message, to=[to_email])
+    send_email.send()
 
     # Send order number and transaction id back to sendData method via JsonResponse
     return render(request, 'orders/payments.html')
