28c876bb4cac044ef45de110b4636dd1f48c483e
prteodoro <97711803+prteodoro@users.noreply.github.com>
2024-08-30 23:09:06 -0300

    Allowed host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['greatkart-firstdeploy-env.eba-9eruvwhs.us-west-2.elasticbeanstalk.com']
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
 
